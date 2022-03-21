9b5c45c8a5c75a18add89598b57be2ecaba896f3
Wesley Freeman <22487192+wesfree163@users.noreply.github.com>
2022-03-20 23:26:01 -0400

    Update Powerball.py

--- Powerball.py
+++ Powerball.py
@@ -6,6 +6,9 @@ Tags: short, humor, simulation"""
 
 import random
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import colors
+from matplotlib.ticker import PercentFormatter
 
 print('''Powerball Lottery, by Al Sweigart al@inventwithpython.com
 
@@ -17,137 +20,313 @@ This simulation gives you the thrill of playing without wasting money.
 ''')
 
 # Let the player enter the fiest five numbers, 1 to 69:
-while True:
+# while True:
 
-    print('How many white balls to use')
-    w_ball_count = int(input('> '))
-
-
-    # GOTTA EDIT THIS PART???
-    print('Range of (whiteBall) number (1 to n, For example 1 to 99)')
-    v_range = int(input('n = '))
+#     print('How many white balls to use')
+#     w_ball_count = int(input('> '))
 
 
-    # remember to adjust to limit to only 2
-    print('How many power balls to use (For example: 0, 1, or 2)')
-    power_ball_count = int(input('> '))
-
-    print('Range of the powerball (For example: 1 to 99)')
-    v_range = int(input('n = '))
+#     # GOTTA EDIT THIS PART???
+#     print('Range of white ball number (1 to n, For example 1 to 99)')
+#     v_range = int(input('n = '))
 
 
-    randnums = np.random.randint(1,v_range,w_ball_count)
+#     # remember to adjust to limit to only 2
+#     print('How many power balls to use (For example: 0, 1, or 2)')
+#     power_ball_count = int(input('> '))
+
+#     print('Range of the powerball (For example: 1 to 99)')
+#     v_range = int(input('n = '))
+
+
+#     randnums = np.random.randint(1,v_range,w_ball_count)
         
-    print(f'Enter {w_ball_count} different numbers from 1 to {v_range}, with spaces between')
-    print(f'each number. (For example: {" ".join((str(i) for i in randnums))})')
-    response = input('> ')
+#     print(f'Enter {w_ball_count} different numbers from 1 to {v_range}, with spaces between')
+#     print(f'each number. (For example: {" ".join((str(i) for i in randnums))})')
+#     response = input('> ')
 
-    print(f'Enter {power_ball_count} different powerball numbers from 1 to {v_range}, with spaces between')
-    print(f'each number. (For example: {" ".join((str(i) for i in randnums))})')
-    response = input('> ')
+#     print(f'Enter {power_ball_count} different powerball numbers from 1 to {v_range}, with spaces between')
+#     print(f'each number. (For example: {" ".join((str(i) for i in randnums))})')
+#     response = input('> ')
 
-    # Check that the player entered correct number of things:
-    numbers = response.split()
-    if len(numbers) != power_ball_count:
-        print(f'Please enter {power_ball_count} powerball numbers, separated by spaces.')
-        continue
+#     # Check that the player entered correct number of things:
+#     numbers = response.split()
+#     if len(numbers) != power_ball_count:
+#         print(f'Please enter {power_ball_count} powerball numbers, separated by spaces.')
+#         continue
 
-    # Convert the strings into integers:
-    try:
-        for i in range(power_ball_count):
-            numbers[i] = int(numbers[i])
-    except ValueError:
-        print('Please enter numbers, like 27, 35, or 62.')
-        continue
+#     # Convert the strings into integers:
+#     try:
+#         for i in range(power_ball_count):
+#             numbers[i] = int(numbers[i])
+#     except ValueError:
+#         print('Please enter numbers, like 27, 35, or 62.')
+#         continue
 
-    # Check that the numbers are between 1 and 69:    
-    for i in range(power_ball_count):
-        if not (1 <= numbers[i] <= v_range):
-            while not (1 <= numbers[i] <= v_range):
-                print('The numbers must all be between 1 and 69.')
-                print(f'Enter {power_ball_count} different numbers from 1 to 69, with spaces between')
-                print('each number. (For example: 5 17 23 42 50)')
-                response = input('> ')
+#     # Check that the numbers are between 1 and 69:    
+#     for i in range(power_ball_count):
+#         if not (1 <= numbers[i] <= v_range):
+#             while not (1 <= numbers[i] <= v_range):
+#                 print('The numbers must all be between 1 and 69.')
+#                 print(f'Enter {power_ball_count} different numbers from 1 to 69, with spaces between')
+#                 print('each number. (For example: 5 17 23 42 50)')
+#                 response = input('> ')
                 
+#             continue
+        
+#     # Check that the numbers are unique:
+#     # (Create a set from number to remove duplicates.)
+#     if len(set(numbers)) != power_ball_count:
+#         print('You must enter {power_ball_count} different numbers.')
+#         continue
+
+#     break
+
+# # Let the player select the powerball, 1 to 26:
+# while True:
+#     print('Enter the powerball number from 1 to 26.')
+#     response = input('> ')
+
+#     # Convert the strings into integers:
+#     try:
+#         powerball = int(response)
+#     except ValueError:
+#         print('Please enter a number, like 3, 15, or 22.')
+#         continue
+
+#     # Check that the number is between 1 and 26:
+#     if not (1 <= powerball <= 26):
+#         print('The powerball number must be between 1 and 26.')
+#         continue
+
+#     break
+
+# # Enter the number of times you want to play:
+# while True:
+#     print('How many times do you want to play? (Max: 1000000)')
+#     response = input('> ')
+
+#     # Convert the strings into integers
+#     try:
+#         numPlays = int(response)
+#     except ValueError:
+#         print('Please enter a number, like 3, 15, or 22000.')
+#         continue
+
+#     # Check that the number is between 1 and 1000000:
+#     if not(1 <= numPlays <= 1000000):
+#         print('You can play between 1 and 1000000 times.')
+#         continue
+
+#     break
+
+# # Run the simulation:
+# price = '$' + str(2 * numPlays)
+# print('It costs', price, 'to play', numPlays, 'times, but don\'t')
+# print('worry. I\'m sure you\'ll win it all back.')
+# input('Press Enter to start...')
+
+# possibleNumbers = list(range(1, 70))
+# for i in range(numPlays):
+#     # Come up with lottery numbers:
+#     random.shuffle(possibleNumbers)
+#     winningNumbers = possibleNumbers[0:power_ball_count]
+#     winningPowerball = random.randint(1, 26)
+
+#     # Display winning numbers:
+#     print('The winning numbers are: ', end='')
+#     allWinningNums = ''
+#     for i in range(power_ball_count):
+#         allWinningNums += str(winningNumbers[i]) + ' '
+#     allWinningNums += 'and ' + str(winningPowerball)
+#     print(allWinningNums.ljust(21), end='')
+
+#     # NOTE: Sets are not ordered, so it doesn't matter what order the
+#     # integers in set(numbers) and set(winningNumbers) are.
+#     if (set(numbers) == set(winningNumbers)
+#         and powerball == winningPowerball):
+#         print()
+#         print('You have won the Powerball Lottery! Congratulations,')
+#         print('you would be a billionaire if this was real!')
+#         break
+#     else:
+#         print(' You lost.') # The leading space is required here
+        
+# print('You have wasted', price)
+# print('Thanks for playing!')
+
+
+def trial_run_Simulation(w_ball_count, v_range, power_ball_count, p_range, p_winning_ball, trials):
+    numbers = []
+    while True:
+
+        print('How many white balls to use')
+        w_ball_count = w_ball_count
+
+
+        # GOTTA EDIT THIS PART???
+        print('Range of white Ball number (1 to n, For example 1 to 99)')
+        v_range = v_range
+
+
+        # remember to adjust to limit to only 2
+        print('How many powerballs to use (For example: 0, 1, or 2)')
+        power_ball_count = power_ball_count
+        power_array = [0] * power_ball_count
+
+        print('Range of the powerball (For example: 1 to 99)')
+        p_range = p_range
+
+
+        randnums = np.random.randint(1,v_range,w_ball_count)
+        some_numbers = [0]
+
+        print(f'Entering {w_ball_count} different numbers from 1 to {v_range}, with spaces between')
+        # print(f'each number. (For example: {" ".join((str(i) for i in randnums))})')
+        num = 0
+        while num < len(some_numbers):
+            some_numbers[num] = random.randint(1, p_range)
+            num += 1
+
+
+        
+        print(f'Entering {power_ball_count} different powerball numbers from 1 to {p_range}, with spaces between')
+        # print(f'each number. (For example: {" ".join((str(i) for i in randnums))})')
+        num = 0
+        while num < len(power_array):
+            power_array[num] = random.randint(1, v_range)
+            num += 1
+
+        # Check that the player entered correct number of things:
+        # for i in some_numbers.count:
+        #     numbers += i.split(", ")
+
+        if len(some_numbers) != power_ball_count:
+            print(f'Please enter {power_ball_count} powerball numbers, separated by spaces.')
             continue
-        
-    # Check that the numbers are unique:
-    # (Create a set from number to remove duplicates.)
-    if len(set(numbers)) != power_ball_count:
-        print('You must enter {power_ball_count} different numbers.')
-        continue
 
-    break
+        # Convert the strings into integers:
+        # try:
+        #     for i in len(numbers):
+        #         numbers[i] = int(numbers[i])
+        # except ValueError:
+        #     print('Please enter numbers, like 27, 35, or 62.')
+        #     continue
 
-# Let the player select the powerball, 1 to 26:
-while True:
-    print('Enter the powerball number from 1 to 26.')
-    response = input('> ')
+        # Check that the numbers are between 1 and 69:    
+        for i in range(power_ball_count):
+            if not (1 <= some_numbers[i] <= v_range):
+                while not (1 <= some_numbers[i] <= v_range):
+                    print(f'The numbers must all be between 1 and {v_range}.')
+                    print(f'Enter {power_ball_count} different numbers from 1 to 69, with spaces between')
+                    print('each number. (For example: 5 17 23 42 50)')
+                    response = input('> ')
+                    
+                continue
+            
+        # Check that the numbers are unique:
+        # (Create a set from number to remove duplicates.)
+        if len(set(some_numbers)) != power_ball_count:
+            print('You must enter {power_ball_count} different numbers.')
+            continue
 
-    # Convert the strings into integers:
-    try:
-        powerball = int(response)
-    except ValueError:
-        print('Please enter a number, like 3, 15, or 22.')
-        continue
-
-    # Check that the number is between 1 and 26:
-    if not (1 <= powerball <= 26):
-        print('The powerball number must be between 1 and 26.')
-        continue
-
-    break
-
-# Enter the number of times you want to play:
-while True:
-    print('How many times do you want to play? (Max: 1000000)')
-    response = input('> ')
-
-    # Convert the strings into integers
-    try:
-        numPlays = int(response)
-    except ValueError:
-        print('Please enter a number, like 3, 15, or 22000.')
-        continue
-
-    # Check that the number is between 1 and 1000000:
-    if not(1 <= numPlays <= 1000000):
-        print('You can play between 1 and 1000000 times.')
-        continue
-
-    break
-
-# Run the simulation:
-price = '$' + str(2 * numPlays)
-print('It costs', price, 'to play', numPlays, 'times, but don\'t')
-print('worry. I\'m sure you\'ll win it all back.')
-input('Press Enter to start...')
-
-possibleNumbers = list(range(1, 70))
-for i in range(numPlays):
-    # Come up with lottery numbers:
-    random.shuffle(possibleNumbers)
-    winningNumbers = possibleNumbers[0:power_ball_count]
-    winningPowerball = random.randint(1, 26)
-
-    # Display winning numbers:
-    print('The winning numbers are: ', end='')
-    allWinningNums = ''
-    for i in range(power_ball_count):
-        allWinningNums += str(winningNumbers[i]) + ' '
-    allWinningNums += 'and ' + str(winningPowerball)
-    print(allWinningNums.ljust(21), end='')
-
-    # NOTE: Sets are not ordered, so it doesn't matter what order the
-    # integers in set(numbers) and set(winningNumbers) are.
-    if (set(numbers) == set(winningNumbers)
-        and powerball == winningPowerball):
-        print()
-        print('You have won the Powerball Lottery! Congratulations,')
-        print('you would be a billionaire if this was real!')
         break
-    else:
-        print(' You lost.') # The leading space is required here
-        
-print('You have wasted', price)
-print('Thanks for playing!')
+
+    # Let the player select the powerball, 1 to 26:
+    while True:
+        print('Enter the powerball number from 1 to 26.')
+        # response = input('> ')
+        powerball = p_winning_ball
+
+        # Convert the strings into integers:
+        # try:
+        #     powerball = int(response)
+        # except ValueError:
+        #     print('Please enter a number, like 3, 15, or 22.')
+        #     continue
+
+        # Check that the number is between 1 and 26:
+        if not (1 <= powerball <= 26):
+            print('The powerball number must be between 1 and 26.')
+            continue
+
+        break
+
+    # Enter the number of times you want to play:
+    while True:
+        print('How many times do you want to play? (Max: 1000000)')
+        trials = trials
+        numPlays = trials
+        # response = input('> ')
+
+        # Convert the strings into integers
+        # try:
+        #     numPlays = int(response)
+        # except ValueError:
+        #     print('Please enter a number, like 3, 15, or 22000.')
+        #     continue
+
+        # Check that the number is between 1 and 1000000:
+        if not(1 <= numPlays <= 1000000):
+            print('You can play between 1 and 1000000 times.')
+            continue
+
+        break
+
+    # Run the simulation:
+    price = '$' + str(2 * numPlays)
+    print('It costs', price, 'to play', numPlays, 'times, but don\'t')
+    print('worry. I\'m sure you\'ll win it all back.')
+    input('Press Enter to start...')
+
+    possibleNumbers = list(range(1, 70))
+    for i in range(numPlays):
+        # Come up with lottery numbers:
+        random.shuffle(possibleNumbers)
+        winningNumbers = possibleNumbers[0:power_ball_count]
+        winningPowerball = random.randint(1, 26)
+
+        # Display winning numbers:
+        print('The winning numbers are: ', end='')
+        allWinningNums = ''
+        for i in range(power_ball_count):
+            allWinningNums += str(winningNumbers[i]) + ' '
+        allWinningNums += 'and ' + str(winningPowerball)
+        print(allWinningNums.ljust(21), end='')
+
+        # NOTE: Sets are not ordered, so it doesn't matter what order the
+        # integers in set(numbers) and set(winningNumbers) are.
+        if (set(numbers) == set(winningNumbers)
+            and powerball == winningPowerball):
+            print()
+            print('You have won the Powerball Lottery! Congratulations,')
+            print('you would be a billionaire if this was real!')
+            break
+        else:
+            print(' You lost.') # The leading space is required here
+            
+    print('You have wasted', price)
+    print('Thanks for playing!\n\n\n')
+
+
+
+wballRange = random.randint(1, 10)
+pballRange = random.randint(1, 5)
+pwinBall = random.randint(1, 26)
+
+trial_run_Simulation(5, wballRange, 1, pballRange, pwinBall, 1000)
+
+
+wballRange = random.randint(1, 15)
+pballRange = random.randint(1, 10)
+pwinBall = random.randint(1, 26)
+
+trial_run_Simulation(3, wballRange, 1, pballRange, pwinBall, 1000)
+
+
+# Need to make program accept 2 powerball inputs
+wballRange = random.randint(1, 10)
+pballRange = random.randint(1, 7)
+pwinBall = random.randint(1, 26)
+
+trial_run_Simulation(2, wballRange, 1, pballRange, pwinBall, 1000)
